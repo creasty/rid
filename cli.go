245ac35e5001d6a17de03471e5dd5cf2c7a52546
Yuki Iwanaga <yuki@creasty.com>
2017-03-13 19:02:12 +0900

    Automatically start services

--- cli.go
+++ cli.go
@@ -9,13 +9,15 @@ import (
 
 type CLI struct {
 	*Context
-	Args []string
+	Args      []string
+	AutoStart bool
 }
 
 func NewCLI(ctx *Context, args []string) *CLI {
 	return &CLI{
-		Context: ctx,
-		Args:    args[1:],
+		Context:   ctx,
+		Args:      args[1:],
+		AutoStart: true,
 	}
 }
 
@@ -27,6 +29,12 @@ func (c *CLI) Run() error {
 	}
 
 	c.substituteCommand()
+
+	if c.AutoStart {
+		if err := c.start(); err != nil {
+			return err
+		}
+	}
 
 	return c.run()
 }
@@ -43,20 +51,30 @@ func (c *CLI) substituteCommand() {
 	switch c.Args[0] {
 	case "compose":
 		c.Args[0] = "docker-compose"
+		c.AutoStart = false
 	default:
 		if cmd, ok := c.Executable[c.Args[0]]; ok {
 			c.Args[0] = cmd
+			c.AutoStart = false
 		}
 	}
 }
 
-func (c *CLI) run() error {
-	cmd := exec.Command(c.Args[0], c.Args[1:]...)
+func (c *CLI) exec(name string, args ...string) error {
+	cmd := exec.Command(name, args...)
 	cmd.Dir = c.BaseDir
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
+}
+
+func (c *CLI) run() error {
+	return c.exec(c.Args[0], c.Args[1:]...)
+}
+
+func (c *CLI) start() error {
+	return c.exec("docker-compose", "up", "-d")
 }
 
 func (c *CLI) ExecHelp() (bool, error) {
