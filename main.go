c26506675c6f5ff74b10ee678855fca8bae0ece5
Yuki Iwanaga <yuki@creasty.com>
2017-03-14 11:05:38 +0900

    Stop using panic

--- main.go
+++ main.go
@@ -9,24 +9,28 @@ import (
 func main() {
 	wd, err := os.Getwd()
 	if err != nil {
-		panic(err)
+		exit(err)
 	}
 
 	ctx, err := NewContext(wd)
 	if err != nil {
-		panic(err)
+		exit(err)
 	}
 
 	cfg, err := NewConfig(ctx.ConfigFile)
 	if err != nil {
-		panic(err)
+		exit(err)
 	}
 
 	cli := NewCLI(ctx, cfg, os.Args)
 	if err := cli.Run(); err != nil {
-		if _, ok := err.(*exec.ExitError); !ok {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		os.Exit(1)
+		exit(err)
 	}
+}
+
+func exit(err error) {
+	if _, ok := err.(*exec.ExitError); !ok {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	os.Exit(1)
 }
